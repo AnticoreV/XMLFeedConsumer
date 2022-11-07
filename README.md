64f161cd736d0a91b8c4c53fb2c7db2b27a72440
AnticoreV <sapronovivancore@gmail.com>
2022-11-07 11:25:17 +0100

    Restructured

---
+++ README.md
@@ -0,0 +1,4 @@
+Manually booting with maven:
+
+mvn exec:java -Dexec.args=“path_to_config_file” -Dexec.mainClass=“path_to_main”
+
--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,7 +1,3 @@
---Database:xml
-
---CREATE DATABASE xml;
---
 --CREATE TABLE element(
 --    id INT PRIMARY KEY AUTO_INCREMENT,
 --    first_el VARCHAR(255),
@@ -10,6 +6,8 @@
 --
 --CREATE TABLE content(
 --    content VARCHAR(1023),
+--    attribute_name VARCHAR(511),
+--    attribute_value VARCHAR(511),
 --    id_el INT PRIMARY KEY AUTO_INCREMENT
 --#     ,FOREIGN KEY (id_el) REFERENCES element (id) ON DELETE CASCADE
 --);
